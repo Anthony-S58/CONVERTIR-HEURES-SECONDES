c8645e72243d374866a6e4d897eae823328d3f1b
Anthony <tonysim01@hotmail.com>
2021-07-23 14:21:54 +0200

    terminé

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let minutes = 60;
 let result = document.getElementById('result');
 let heures = document.querySelector('input');
 
-console.log(heures.value);
+// console.log(heures.value);
 
 result.innerHTML= heures.value*minutes*secondes +' secondes';
 
